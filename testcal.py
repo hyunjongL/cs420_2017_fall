d4316dc179baca9875684f1afb58827b85b9e810
hyunjongL <hyunjongl@kaist.ac.kr>
2017-11-27 13:29:00 +0900

    caltesdtxi

---
+++ testcal.py
@@ -0,0 +1,142 @@
+
+
+# -----------------------------------------------------------------------------
+# calc.py
+#
+# A simple calculator with variables -- all in one file.
+# -----------------------------------------------------------------------------
+
+tokens = (
+    'NAME','NUMBER','FLOAT',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'LPAREN','RPAREN','SCOLON',
+    )
+
+# Tokens
+
+t_SCOLON  = r';'
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQUALS  = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+def t_FLOAT(t):
+    r'-?\d+\.\d'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print('error haha')
+        t.value = 0
+    return t
+
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+
+# Ignored characters
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lexer = lex.lex()
+
+# Parsing rules
+
+precedence = (
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS'),
+    )
+
+# dictionary of names
+names = { }
+
+# dictionary of functions
+funcs = { }
+
+def p_statement_declare(t):
+    'statement : NAME NAME SCOLON'
+    names[t[2]] = {'type' : t[1], 'value': None}
+
+
+def p_statement_assign(t):
+    'statement : NAME EQUALS expression'
+    names[t[1]] = t[3]
+
+
+def p_statement_expr(t):
+    'statement : expression'
+    print(t[1])
+
+def p_func(t):
+    'expression : NAME LPAREN expression RPAREN '
+    if t[1] == 'print':
+        print(t[3])
+    elif t[1] == 'float':
+        t[0]= (float(t[3]))
+    elif t[1] == 'int':
+        t[0]= (int(t[3]))
+
+def p_expression_binop(t):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if t[2] == '+'  : t[0] = t[1] + t[3]
+    elif t[2] == '-': t[0] = t[1] - t[3]
+    elif t[2] == '*': t[0] = t[1] * t[3]
+    elif t[2] == '/': t[0] = t[1] / t[3]
+
+def p_expression_uminus(t):
+    'expression : MINUS expression %prec UMINUS'
+    t[0] = -t[2]
+
+def p_expression_group(t):
+    'expression : LPAREN expression RPAREN'
+    t[0] = t[2]
+
+def p_expression_number(t):
+    '''expression : NUMBER
+                  | FLOAT'''
+    t[0] = t[1]
+
+def p_expression_name(t):
+    'expression : NAME'
+    try:
+        t[0] = names[t[1]]
+    except LookupError:
+        print("Undefined name '%s'" % t[1])
+        t[0] = 0
+
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('calc > ')   # Use raw_input on Python 2
+    except EOFError:
+        break
+    parser.parse(s)
